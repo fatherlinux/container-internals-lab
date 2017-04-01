88f69b16b272ebd30d456940be83e07002fa221b
root <root@keith.dc2.crunchtools.com>
2017-04-01 02:41:19 -0400

    SM: Updated export portion of script

--- meta/lab-build/lab-operations.sh
+++ meta/lab-build/lab-operations.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ocp_servers="master1 master2 master2 node1 node2 node3"
+ocp_servers="master1 master2 master3 node1 node2 node3"
 all_servers="$ocp_servers storage1 haproxy1"
 domain="ocp1.dc2.crunchtools.com"
 ssh_options="-oStrictHostKeyChecking=no -i ./root@ocp1.dc2.crunchtools.com"
@@ -23,9 +23,34 @@ prep)
         ssh $ssh_options root@${server}.${domain} docker pull rhel7
         ssh $ssh_options root@${server}.${domain} docker pull rhel7/rhel-tools
         ssh $ssh_options root@${server}.${domain} docker pull nate/dockviz
+        ssh $ssh_options root@${server}.${domain} docker pull openshift3/ose-haproxy-router
+        ssh $ssh_options root@${server}.${domain} docker pull openshift3/ose-docker-registry
+        ssh $ssh_options root@${server}.${domain} docker pull openshift3/registry-console
     done
   ;;
 export)
+    input_directory="/dev/RHEL7CSB"
+    output_directory="/run/media/fatherlinux/passport-1TB/tmp/container-internals-lab"
+
+    for server in $all_servers
+    do
+        virsh dumpxml ${server}.${domain} > ${output_directory}/L103118-${server}.${domain}.xml
+        sed -e "#<uuid>.*</uuid>$#d" -i ${output_directory}/L103118-${server}.${domain}.xml
+        sed -e "s#${input_directory}/${server}.${domain}#${output_directory}/L103118-${server}.${domain}.raw#" -i ${output_directory}/L103118-${server}.${domain}.xml
+        sed -e "s#${server}.${domain}#L103118-${server}.${domain}#" -i ${output_directory}/L103118-${server}.${domain}.xml
+    done
+
+    for server in $all_servers
+    do
+        # dd if=${input_directory}/${server}.${domain} of=${output_directory}/L103118-${server}.${domain}.raw
+	echo "dd if=${input_directory}/${server}.${domain} of=${output_directory}/L103118-${server}.${domain}.raw"
+    done
+  ;;
+command)
+    for server in $all_servers
+    do
+        ssh $ssh_options root@${server}.${domain} $2
+    done
   ;;
 *)
   echo "Usage: container-processes {test|prep|export}"
