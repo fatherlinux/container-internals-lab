20e7376bb149c70c7e84cbfda06cc4c0b78e724a
root <root@master1.ocp1.dc2.crunchtools.com>
2017-04-15 20:30:10 +0000

    Removed labs/lab-04

--- labs/lab-04/exercise-01/delete.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Deletes the application objects
-oc delete rc/wordpress rc/mysql pvc/claim-wp pvc/claim-mysql routes/wpfrontend svc/mysql svc/wpfrontend
-
-# Deletes the storage objects. Can be commented out to maintain data between instantiations.
-#oc delete pv/pv0001 pv/pv0002 pv/pv0003 pv/pv0004
