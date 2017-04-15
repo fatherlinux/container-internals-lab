3a6378787f3ef524a68e808f03127d3783aef388
root <root@master1.ocp1.dc2.crunchtools.com>
2017-04-15 18:44:00 +0000

    SM: Updated with goodbad logic

--- labs/lab-04/excercise-02/index.php
+++ labs/lab-04/excercise-02/index.php
@@ -3,6 +3,17 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php 
+	$myfile = fopen("goodbad.txt", "r") or die("Unable to open file!");
+	i$goodbad = fread($myfile,filesize("goodbad.txt"));
+	fclose($myfile);
+	if ($goodbad == 1) {
+		echo '<p>Hello World</p>';
+	}
+	else {
+		echo '<p>500 ERROR</p>';
+		header($_SERVER['SERVER_PROTOCOL'] . ' 500 Internal Server Error', true, 500);
+	}
+?>
  </body>
 </html>
